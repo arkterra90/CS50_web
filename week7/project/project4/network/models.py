37af4d716c307d239a40f9496b149448b685fd45
Jeremy Kuehn <100542455+arkterra90@users.noreply.github.com>
2023-09-18 15:11:49 -0500

    Starts Final Project

---
+++ week7/project/project4/network/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(verbose_name="Post Text")
+    timeStamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.id}: {self.user} {self.text} {self.timeStamp}"
+    
+    @classmethod
+    def create_post(cls, user, text):
+        post = cls(user=user, text=text)
+        post.save()
+        return post
---
+++ week7/project/project4/network/views.py
@@ -0,0 +1,88 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.core.paginator import Paginator
+
+from .models import *
+
+
+def index(request):
+
+    allPost = Post.objects.all().order_by('-timeStamp')
+    p =  Paginator(allPost, 2)
+    page_number = 1
+    page = p.get_page(page_number)
+
+    
+    return render(request, "network/index.html", {
+        "allPost": allPost,
+        "p": page
+    })
+
+def post(request):
+    if request.method == "POST":
+        text = request.POST.get("newPostBox")
+        if text:
+            try:
+                Post.create_post(user=request.user, text=text)
+            except IntegrityError:
+                return render(request, "network/index.html", {
+                    "message": "Could Not Create New Post"
+                })
+
+    return render(request, "network/index.html")
+
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "network/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "network/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "network/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "network/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "network/register.html")
